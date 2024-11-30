5e8ba69454dece9ac5154eaf21af774659da10e4
Jordan J <jordanr2m@gmail.com>
2024-11-29 17:33:30 -0700

    Added API call. Created components folder and Header component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  // console.log(movies);
+  const [isLoading, setIsLoading] = useState(false);
+  const ghibliUrl = "https://ghibliapi.vercel.app/films";
+
+  const fetchMovies = async () => {
+    try {
+      setIsLoading(true);
+      const request = await fetch(ghibliUrl);
+      const results = await request.json();
+      setMovies(results);
+    } catch (err) {
+      console.log("Something went wrong.", err);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <h1>Movie App</h1>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Studio Ghibli Film Finder</h1>
+    </header>
+  )
+}
+
+export default Header
