b8a8c4137b2fa18681e061ec0a00049cadb937e0
Jordan J <jordanr2m@gmail.com>
2024-12-05 15:19:02 -0800

    Worked on header styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,13 @@ const Header = () => {
   return (
     <header>
       <div className="wrapper">
-        <img src={logo} alt="Studio Ghibli Logo"/>
-        <h1>Studio Ghibli Film Finder</h1>
-        <h2>Find your next Ghibli film</h2>
+        <div className='logo-wrapper'>
+          <img src={logo} alt="Studio Ghibli Logo" />
+        </div>
+        <div className='header-text'>
+          <h1>Studio Ghibli Film Finder</h1>
+          <h2>Find your next Ghibli film</h2>
+        </div>
       </div>
     </header>
   );
