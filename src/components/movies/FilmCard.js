a936718a8b22e2c807da30d31314c5b0992af059
Jordan J <jordanr2m@gmail.com>
2024-12-07 12:16:41 -0800

    Continued working on film card design.

--- src/components/movies/FilmCard.js
+++ src/components/movies/FilmCard.js
@@ -1,6 +1,15 @@
 
 
 const FilmCard = ({ movie }) => {
+
+    const convertRuntime = (minutes) => {
+        let hours = Math.floor(minutes / 60);
+        let remainingMinutes = minutes % 60;
+        // remainingMinutes = remainingMinutes < 10 ? `0${remainingMinutes}` : remainingMinutes;
+        return `${hours} ${hours > 1 ? "hours" : "hour"} ${remainingMinutes} ${remainingMinutes === 1 ? "minute" : "minutes"}`
+        // return `${hours}:${remainingMinutes}`
+    }
+
     return (
         <article className="film-card">
             <div className="film-card-content">
@@ -10,6 +19,7 @@ const FilmCard = ({ movie }) => {
                 <div className="film-info">
                     <h3>{movie.title} <span>({movie.release_date})</span></h3>
                     <p>Directed by: {movie.director}</p>
+                    <p>Runtime: {convertRuntime(movie.running_time)}</p>
                 </div>
             </div>
         </article>
