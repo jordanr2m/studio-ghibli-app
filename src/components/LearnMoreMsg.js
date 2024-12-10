632312be319c15d490f11c1866d9f5bc92666d0a
Jordan J <jordanr2m@gmail.com>
2024-12-10 15:03:07 -0800

    Minor style updates.

--- src/components/LearnMoreMsg.js
+++ src/components/LearnMoreMsg.js
@@ -13,7 +13,8 @@ const LearnMoreMsg = ({ removeMessage }) => {
                 >
                     <IoClose />
                 </button>
-                <p>Studio Ghibli, Inc. is a Japanese animation studio based in Koganei, Tokyo. It was founded on June 15, 1985, by the directors Hayao Miyazaki and Isao Takahata and producer Toshio Suzuki. Studio Ghibli has a strong presence in the animation industry. Their work has been well received by audiences around the world and recognized with numerous awards. Studio Ghibli's mascot and most recognizable symbol, the character Totoro from the 1988 film My Neighbor Totoro, is a giant spirit inspired by raccoon dogs (<i>tanuki</i>) and cats (<i>neko</i>). Among the studio's highest-grossing films are Princess Mononoke (1997), Spirited Away (2001), Howl's Moving Castle (2004), and Ponyo (2008).</p>
+                
+                <p>Studio Ghibli, Inc. is a Japanese animation studio based in Koganei, Tokyo. It was founded on June 15, 1985, by directors, Hayao Miyazaki and Isao Takahata, and producer Toshio Suzuki. Studio Ghibli has a strong presence in the animation industry. Their work has been well received by audiences around the world and recognized with numerous awards. Studio Ghibli's mascot and most recognizable symbol, the character Totoro from the 1988 film, <i>My Neighbor Totoro</i>, is a giant spirit inspired by raccoon dogs (<i>tanuki</i>) and cats (<i>neko</i>). Among the studio's highest-grossing films are: <i>Princess Mononoke</i> (1997), <i>Spirited Away</i> (2001), <i>Howl's Moving Castle</i> (2004), and <i>Ponyo</i> (2008).</p>
                 
                 <div className="msg-img">
                     <img className="msg-img" src={sootSprite1} alt="Dancing soot sprites with stars"/>
