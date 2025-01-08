58c1f6fd4f41fcf44d0fbd034e5511e6f3c56cbd
Jordan J <jordanr2m@gmail.com>
2025-01-08 14:51:36 -0800

    Experimenting with desktop styles. Added icon to site footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
+import footerIcon from '../imgs/icons8-totoro-100.png';
+
 const Footer = () => {
   return (
     <footer className="site-footer">
-      &copy; {new Date().getFullYear()} || Site by <a href="https://github.com/jordanr2m" target="blank">Jordan Jackson</a>
+      <img src={footerIcon} alt='Totoro icon'/>
+      <div>
+        &copy; {new Date().getFullYear()} || Site by <a href="https://github.com/jordanr2m" target="blank">Jordan Jackson</a>
+      </div>
     </footer>
   )
 }
